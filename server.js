9941a3ff71203bcb3857d534d77cc6aeb54cd905
Glitch (leather-weeder) <none>
2018-07-03 10:49:45 +0000

    🐂👛 Updated with Glitch

--- server.js
+++ server.js
@@ -1,19 +1,56 @@
-//server.js
+// server.js
+// where your node app starts
 
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+// init project
+var express = require('express');
+var app = express();
+var moment = require('moment');
 
-const app = express();
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-const port = 8000;
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
-//how we imported our routes into our server
-require('./app/routes')(app, {});
-
-app.listen(port, ()=>{
-    console.log(`We are live on ${port}`);
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
 
+// This is the first test endpoint for the FCC project
+app.get('/api/timestamp', (req, res)=>{
+  var currentEpoch = Math.round(new Date().getTime());
+  var currentDate = new Date().toUTCString();
+  res.json({'unix': currentEpoch, 'utc': currentDate});
+});
+
+
+app.get('/api/timestamp/:data_string', (req, res)=>{
+  let stringDate = Number(req.params.data_string);
+  console.log(stringDate);
+
+  if(!isNaN(stringDate)){
+    let isoDate = moment.unix(stringDate).format("ddd, MMMM Do YYYY, h:mm:ss a");
+    console.log(isoDate);
+    res.json({'unix': stringDate, 'utc': isoDate });
+  } else if(moment(req.params.data_string)){
+    res.json({'unix': moment(req.params.data_string).unix(), 'utc':moment(req.params.data_string).format("ddd, MMMM Do YYYY, h:mm:ss a")})
+  } else{
+    res.json({"unix": null, "utc" : "Invalid Date" });
+  }
+
+  });
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});;
